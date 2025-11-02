bfa5d058f11c1a0b31a96545fc4c62c3f06fb519
ShahadatGithub <shahadat.offl@gmail.com>
2025-11-02 23:48:16 +0530

    updated env

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://whitehilll.com"],
+    origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // ✅ allow cookies / credentials to be sent
